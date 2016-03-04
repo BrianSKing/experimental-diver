baf5c03f7d2169bcd27ed8348eb40417ed4fc756
BrianSKing <Dev@Brians-MBP.home>
2016-03-03 22:23:15 -0800

    Added recursive component and refined strings
    recursive diver.  to use set max depth when calling component:
    
    {{recursive-diver maxDepth=whatever}}
    
    that many nested components will be recursively created, each within an
    {{#if evaluatesToTrue}}

--- app/components/recursive-diver.js
+++ app/components/recursive-diver.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  maxDepth: null,
+  currentLevel: 0,
+
+  nextLevel: function() {
+    return this.get('currentLevel') + 1;
+  }.property('currentLevel'),
+
+  shouldRecur: function() {
+    console.log(this.get('currentLevel') + ' : ' + this.get('maxDepth'));
+    if (this.get('currentLevel') >= this.get('maxDepth')) {
+      return false;
+    }
+    return true;
+  }.property('maxDepth', 'currentLevel')
 });
